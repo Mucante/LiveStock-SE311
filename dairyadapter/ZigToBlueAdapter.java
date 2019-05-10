de599df7ae39a12cbf0ec18bb164c7578aeb2509
Batuhan Üzümcü <batuhan.uzumcu@std.izmirekonomi.edu.tr>
2019-05-10 22:26:05 +0300

    Update ZigToBlueAdapter.java

--- dairyadapter/ZigToBlueAdapter.java
+++ dairyadapter/ZigToBlueAdapter.java
@@ -3,10 +3,10 @@ package dairyadapter;
 public class ZigToBlueAdapter implements BluetoothSignal{
 
 	public double usingBluetoothSignal(double newrange) {
-		// Possibly do some other work and then call
-		// usingEURSocket from the European socket.
+		
 		double range = _adaptee.usingZigbeeSignal(newrange);
 		return range;
+
 	}
 	
 	public ZigToBlueAdapter (ZigbeeSignal adaptee) {
