b552bd4e8fa12d94b78998f2d0726b72831ff21e
Can Özer <45624149+Mucante@users.noreply.github.com>
2019-05-16 16:37:19 +0300

    Add files via upload

--- livestocksingleton/Database.java
+++ livestocksingleton/Database.java
@@ -1,1 +1,38 @@
+package livestocksingleton;
+import java.util.ArrayList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
+import livestockobserver.TrackingDevice;
+
+public class Database {
+	public static Database GetDatabase(ArrayList<TrackingDevice> newtrackers) {
+		
+		if (instance == null) {
+			lock.lock();
+			//System.out.println("Acquired lock");
+			try {
+				if (instance == null)
+					instance = new Database(newtrackers);
+			} finally {
+				lock.unlock();
+				//System.out.println("Released lock");
+			}
+		}
+		return instance;
+	}
+
+	//gets the location of the cattle
+	public TrackingDevice getTrackingDevice(int id) {	
+				return trackers.get(id);
+	}	
+
+	// Constructor (private).
+	private Database(ArrayList<TrackingDevice> currenttrackers) {
+	      trackers = currenttrackers;
+	}
+
+	private static Database instance = null;
+	private static final Lock lock = new ReentrantLock();
+	private ArrayList<TrackingDevice> trackers;
+}
